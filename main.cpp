81d43272fd19905e9cb51c592b0c3ec5d0072ef7
erfan <erfanramezany245@gmail.com>
2022-11-13 19:37:36 +0330

    modularity reshape

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// in the name of the Lord :: ALLAH
+
+#include </mnt/01D8769274F3FF00/cpp-projects/tic-tac-toe-Ai/Built.h>
+
+int main()
+{
+    Base *exe = new Base;
+
+    exe->start_game();
+
+    return 0;
+}
